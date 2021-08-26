4a52313bbc6ff57f75b47658c0e9c7572c30f25f
2212021 <89271129+2212021@users.noreply.github.com>
2021-08-26 14:22:09 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,56 @@
+import ReactDOM from 'react-dom';
+import React, {Component} from 'react';
 import './App.css';
+
+class Menu extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      show: false
+    };
+  }
+  toggle = () => {
+    this.setState({show:!this.state.show});
+  }
+  menuhead = function() {
+    return (
+      <div onClick={this.toggle}>
+        <h2>MENU</h2>
+      </div>
+    );
+  }
+  menubody = function() {
+    return (
+      <div onMouseLeave={this.hide}>
+        <ul>
+          <a href="#"><li>item1</li></a>
+          <a href="#"><li>item2</li></a>
+        </ul>
+      </div>
+    );
+  }
+  render = function() {
+    if (this.state.show) {
+      return (
+        <div>
+          {this.menuhead()}
+          {this.menubody()}
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          {this.menuhead()}
+        </div>
+      );
+    }
+  }
+}
 
 function App() {
   return (
     <div className="App">
-
+      <Menu/>
     </div>
   );
 }
