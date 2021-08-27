2fe6c5dcc2290b3e825c45dec586b068f9dfa092
2212021 <89271129+2212021@users.noreply.github.com>
2021-08-27 20:54:20 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ class Menu extends Component {
   }
   menubody = function() {
     return (
-      <div class='menu' style=onMouseEnter={this.show} onMouseLeave={this.hide}>
+      <div class='menu' onMouseEnter={this.show} onMouseLeave={this.hide}>
         <div class='menuitem' onClick={this.action1}><h2>Item1</h2></div>
         <div class='menuitem' onClick={this.action1}><h2>Item2</h2></div>
       </div>
